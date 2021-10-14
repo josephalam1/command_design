3118f73286727148b3e43269c8d73c5e2426f90e
Ben dela Pena <bdelapena120@gmail.com>
2021-10-13 21:35:26 -0400

    InputHandler doc

--- InputHandler.java
+++ InputHandler.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
-
+/**
+ * Chooses a command to use from an array list of commands
+ * @author Ben dela Pena
+ */
 public class InputHandler {
 
     private ArrayList<Command> commands;
-
+   /**
+    * Creates the different commands and adds them to an array list of commands
+    * @param cadence To play the song
+    */
     public InputHandler(MilitaryCadence cadence){
         commands = new ArrayList<Command>();
         Command command1 = new IDontKnowCommand();
@@ -13,6 +19,11 @@ public class InputHandler {
         commands.add(command2);
         commands.add(command3);
     }
+    /**
+     * Checks if the user input is valid and exectues the command
+     * @param num the choice of the user of the command
+     * @return true if valid and exectued and false if not
+     */
     public boolean playCadence(int num){
         if (num >-1 && num < 4){
             commands.get(num).execute();
