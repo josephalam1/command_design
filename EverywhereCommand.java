d937b2b8d319b5aa254cab1af5455225aaf9cba7
Joey Alam <jaalam@email.sc.edu>
2021-10-13 21:11:28 -0400

    Added commands

---
+++ EverywhereCommand.java
@@ -0,0 +1,38 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.concurrent.*;
+
+/**
+ * IDontKnowCommand class that handles executing the I Don't Know song
+ * @author Joseph Alam
+ */
+public class EverywhereCommand implements Command {
+
+    /**
+     * Execute - handles playing the I Don't Know song
+     */
+    public void execute() {
+        try {
+            File myObj = new File("everywhere.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            try {
+                System.out.println(data);
+                TimeUnit.MILLISECONDS.sleep(100);
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            } 
+            catch (InterruptedException e) {
+                System.out.println("There was an error while sleeping");
+            }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
